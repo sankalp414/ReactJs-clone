ef30976d396cc90885928ad00ba98331c356741d
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2024-01-19 10:12:35 +0000

    youtube-clone

---
+++ youtube-clone/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <div className='w-full h-screen bg-green-500  text-white '>
+        <h1>
+          hello
+        </h1>
+       </div>
+       
+    </>
+  )
+}
+
+export default App
