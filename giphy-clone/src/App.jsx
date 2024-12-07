373c09939bf38c14b2c19b715dd2ca2326e4b175
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2024-12-07 07:51:02 +0000

    giphy clone setup

---
+++ giphy-clone/src/App.jsx
@@ -0,0 +1,17 @@
+
+import {createBrowserRouter} from "react-router-dom"
+import './App.css'
+
+function App() {
+  const router = createBrowserRouter([{
+    element:<AppLayout/>
+  }])
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
